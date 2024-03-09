370b71d84b9261d86c83ba6c9c78af83ece55bed
GyanCoverQ <gyan@coverq.io>
2024-03-09 16:45:34 +0530

    Second commit

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+let quotesDbURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
 function App() {
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [quotes, setQuotes] = useState("Quotes");
+  const [author, setAuthor] = useState(null);
+  const [quotesArray, setQuotesArray] = useState(null)
+
+  const generateRandomNumber = () =>{
+    let randomInteger = (Math.floor(Math.random()*quotesArray.length))
+    setRandomNumber(randomInteger)
+    setQuotes(quotesArray[randomInteger].quote)
+    setAuthor(quotesArray[randomInteger].author)
+  }
+
+  const fetchQuote = async(url) => {
+    const response = await fetch(url)
+    const parsedJSON = await response.json()
+
+    setQuotesArray(parsedJSON.quotes)
+    console.log(parsedJSON)
+    console.log(typeof(parsedJSON))
+      
+  }
+
+  useEffect(() =>{
+    fetchQuote(quotesDbURL)
+    
+  }, [quotesDbURL])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <p>{quotes}</p>
+      <p>{author}</p>
+      <p>{randomNumber}</p>
+      <button onClick={generateRandomNumber}>Click</button>
+    
     </div>
   );
 }
